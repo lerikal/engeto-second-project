576a25c525e88b231e8cb3344d6ca59d54964afe
lerikal <valeria.listkova@gmail.com>
2026-05-09 14:19:51 +0200

    Initial commit

---
+++ src/main/java/com/engeto/engeto_second_project/EngetoSecondProjectApplication.java
@@ -0,0 +1,13 @@
+package com.engeto.engeto_second_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngetoSecondProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngetoSecondProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engeto/engeto_second_project/EngetoSecondProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engeto.engeto_second_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngetoSecondProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
